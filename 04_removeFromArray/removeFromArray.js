c63f1dc35d57644a4e2560c98815816806f62bd9
linthalo <vincentahluwalia@protonmail.com>
2025-03-07 12:36:46 +0100

    Finish exercise 04, skipped multiple of the same value test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elementToBeRemoved) {
+    for (let i = 0; i < arguments.length; ++i) {
+        for (let j = 0; j < array.length; ++j) {
+            if (arguments[i] === array[j]) {
+                array.splice(j, 1);
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
