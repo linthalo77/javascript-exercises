b9871d73ce23b33b655e07b1e1e569d1740befd5
linthalo <vincentahluwalia@protonmail.com>
2025-03-07 19:18:34 +0100

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5/9);    // Formula for conversion
+  celsius = Math.round(celsius * 10) / 10     // ROund to 1 decimal
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius * (9/5) + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10
+  return fahrenheit;
 };
 
 // Do not edit below this line
