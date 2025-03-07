01c566505a8edbe61d981436730eee5f2b8b7d25
linthalo <vincentahluwalia@protonmail.com>
2025-03-07 11:54:55 +0100

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = "";
+    for (let i = string.length; i >= 0; --i) {
+        result += string.charAt(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
