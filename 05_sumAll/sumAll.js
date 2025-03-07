61ac14b2e5b95832e2d8d9db56b5ef36863881f2
linthalo <vincentahluwalia@protonmail.com>
2025-03-07 18:41:14 +0100

    Remove whitespace

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(num1, num2) {
     if (num1 < 0 || num2 < 0)
         return "ERROR";
    
-    if ( typeof num1 != "number" && typeof num1 != "bigint" || typeof num2 != "number" && typeof num2 != "bigint" )
+    if (typeof num1 != "number" && typeof num1 != "bigint" || typeof num2 != "number" && typeof num2 != "bigint")
         return "ERROR";
 
     if (num1 % 1 != 0 || num2 % 1 != 0) 
