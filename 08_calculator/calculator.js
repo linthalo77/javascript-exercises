09ec6bfeeefe918a00ed7d0d1449f6a7f9835793
linthalo <vincentahluwalia@protonmail.com>
2025-03-11 12:19:28 +0100

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0)
+    return 0;
+
+	return array.reduce((total, current) => total + current);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let temp = 1;
+	for(let i = power; i > 0; --i) {
+    temp = temp * num;
+  }
+  return temp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0)
+    return 1;
+
+  let array = [];
+	for (let i = 1; i <= num; ++i) {
+    array.push(i);
+  }
+  return multiply(array);
 };
 
 // Do not edit below this line
