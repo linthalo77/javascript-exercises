f1060f55cb8f24e4353e57867ee73182195b00c1
linthalo <vincentahluwalia@protonmail.com>
2025-03-11 15:20:55 +0100

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    const currentYear = 2025;
+    let oldestAge = 0;
+    let oldestPerson;
 
+    peopleArr.map((person) => {
+        let age;
+        if("yearOfDeath" in person) 
+            age = person.yearOfDeath - person.yearOfBirth;
+        else 
+            age = currentYear - person.yearOfBirth;
+
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    })
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
