9a822cec76e1ea5763183f5b7fcda2c5f21b8f06
linthalo <vincentahluwalia@protonmail.com>
2025-03-11 14:02:55 +0100

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = +index;
+    if(index < 0)
+        return "OOPS";
+    else if (index == 0)
+        return 0;
 
+    let fibonacci = [1, 1];
+    for (let i = fibonacci.length - 1; i < index - 1; ++i) {
+        fibonacci.push(fibonacci[i] + fibonacci[i - 1]);
+    }
+    console.log(fibonacci);
+    return fibonacci[index - 1];
 };
 
 // Do not edit below this line
